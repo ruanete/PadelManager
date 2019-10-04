1d36cdb3e721e2e3f3f93f39c671431c7cc4446d
Raúl Ruano Narváez <raulruanonarvaez@gmail.com>
2019-10-04 20:06:44 +0200

    Añadido proyecto base

---
+++ padelmanager/src/main/java/com/ruanete/padelmanager/PadelmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ruanete.padelmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadelmanagerApplication.class, args);
+	}
+
+}
