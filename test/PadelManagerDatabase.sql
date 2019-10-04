8731861991d8e5e87d03e6e99331e802dc44b3ac
Raúl Ruano Narváez <raulruanonarvaez@gmail.com>
2019-10-04 23:13:36 +0200

    Añadidos cambios en el backup de la DB

--- test/PadelManagerDatabase.sql
+++ test/PadelManagerDatabase.sql
@@ -19,6 +19,13 @@
 -- Table structure for table `Matches`
 --
 
+/*Create Testuser*/
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+/*Create DB*/
+CREATE DATABASE IF NOT EXISTS `PadelManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `PadelManager`;
+
 DROP TABLE IF EXISTS `Matches`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
