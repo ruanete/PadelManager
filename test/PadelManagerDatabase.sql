41cdf3fcdebfb6a91fbadff023b61b740cc04788
Raúl Ruano Narváez <raulruanonarvaez@gmail.com>
2019-10-06 12:53:23 +0200

    Prueba añadir privilegios root

--- test/PadelManagerDatabase.sql
+++ test/PadelManagerDatabase.sql
@@ -1,6 +1,7 @@
 /*Change root password to use the same BD as in local*/
 use mysql;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Ruanete1997.';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 
 /*Create database*/
 CREATE DATABASE IF NOT EXISTS `PadelManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
