98e99822191ae9600ce41a8c08624f1965d1acbb
Raúl Ruano Narváez <raulruanonarvaez@gmail.com>
2019-10-06 13:18:23 +0200

    Modificadas contraseñas Mysql

--- test/PadelManagerDatabase.sql
+++ test/PadelManagerDatabase.sql
@@ -1,6 +1,12 @@
 /*Change root password to use the same BD as in local*/
 use mysql;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Ruanete1997.';
+SET GLOBAL validate_password.LENGTH = 4;
+SET GLOBAL validate_password.policy = 0;
+SET GLOBAL validate_password.mixed_case_count = 0;
+SET GLOBAL validate_password.number_count = 0;
+SET GLOBAL validate_password.special_char_count = 0;
+SET GLOBAL validate_password.check_user_name = 0;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
 
 /*Create database*/
 CREATE DATABASE IF NOT EXISTS `PadelManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
