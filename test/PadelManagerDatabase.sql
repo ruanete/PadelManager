ea0f489fc6a65e7322a3b57dba0b87ca8b6883ee
Raúl Ruano Narváez <raulruanonarvaez@gmail.com>
2019-10-06 12:56:31 +0200

    Modificado fichero configuracion mysql

--- test/PadelManagerDatabase.sql
+++ test/PadelManagerDatabase.sql
@@ -1,7 +1,6 @@
 /*Change root password to use the same BD as in local*/
 use mysql;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Ruanete1997.';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 
 /*Create database*/
 CREATE DATABASE IF NOT EXISTS `PadelManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
